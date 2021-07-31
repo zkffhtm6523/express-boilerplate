87d6f4601b8390d71b486f44209b53e8e238878e
David <zkffhtm6523@naver.com>
2021-07-31 19:44:28 +0900

    User 스키마 생성

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = mongoose.Schema({
+	name:{
+		type : String,
+		maxlength: 50
+	},
+	email:{
+		type : String,
+		trim : true,
+		unique : 1
+	},
+	lastname: {
+		type : String,
+		maxlength : 50	
+	},
+	password:{
+		type : String,
+		minLength: 5
+	},
+	role : {
+		type : Number,
+		default : 0
+	},
+	image : String,
+	token : {
+		type : String
+	},
+	tokenExp : {
+		type : Number
+	}
+})
+// 스키마를 모델로 감싸준다
+const User = mongoose.model('User', userSchema)
+
+// 다른 곳에서 사용할 수 있게 export를 해준다, 마치 java의 return처럼
+module.exports = { user }
