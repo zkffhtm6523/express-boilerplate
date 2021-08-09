26191b34acf374f90ab0cd17f4d1768fea0db8c6
David <zkffhtm6523@naver.com>
2021-08-10 01:07:47 +0900

    Flow & Axios(#21)

--- server/index.js
+++ server/index.js
@@ -28,6 +28,11 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World.....'))
 
+app.get('/api/hello', (req, res) => {
+
+    res.send("안녕하세요")
+})
+
 // 회원가입
 app.post('/api/users/register', (req, res) => {
 
