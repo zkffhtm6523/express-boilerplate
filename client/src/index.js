a172cefb546cc6909d98dec6da6a22dbd2614951
David <zkffhtm6523@naver.com>
2021-08-14 20:31:55 +0900

    #27 Redux Up

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
-import { applyMiddleware } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers'
