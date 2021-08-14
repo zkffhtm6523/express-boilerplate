6019c53eea6619c606268e72c06bbcf477c820b4
David <zkffhtm6523@naver.com>
2021-08-14 20:58:43 +0900

    #29 로그인 페이지 1

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginPage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        
+    }
+
     return (
-        <div>
-            LoginPage
-        </div>
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems:'center',
+            width: '100%', height: '100vh'
+        }}>
+
+            <form style={{display: 'flex', flexDirection:'column'}}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                {/* value 부분에 prop & state를 넣어줘야 한다 */}
+                <input type="email" value={Email} onChange={onEmailHandler}/>
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler}/>
+                <br/>
+                <button type="submit">
+                    Login
+                </button>
+            </form>
+        </div> 
     )
 }
 
